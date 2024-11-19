8d4f972a90253176d3d8ce9c819ffa812b0d0b62
Michal465 <m_mana@utb.cz>
2024-11-19 14:20:59 +0100

    Startovni

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+    private var counter = 0 // Proměnná pro uchování počítadla
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Připojení prvků z XML
+        val counterText: TextView = findViewById(R.id.counterText)
+        val incrementButton: Button = findViewById(R.id.incrementButton)
+
+        // Nastavení kliknutí na tlačítko
+        incrementButton.setOnClickListener {
+            counter++ // Zvyšte počítadlo
+            counterText.text = counter.toString() // Aktualizujte zobrazený text
+        }
+    }
+}
